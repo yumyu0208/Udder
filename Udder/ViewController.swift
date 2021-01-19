586c0e61b72a415b56af126f3c1601edc63bb44c
Yuma Takeda <yumyu0208@gmail.com>
2021-01-19 12:58:09 +0900

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by 竹田悠真 on 2021/01/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
